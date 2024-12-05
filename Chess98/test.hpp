91d8003ee8686a1aa5a935623f259d8495622fb1
HeliumAreFlying <1436980420@qq.com>
2024-12-05 16:41:44 +0800

    new test

--- Chess98/test.hpp
+++ Chess98/test.hpp
@@ -7,14 +7,46 @@ void standardTest(TEAM team = BLACK)
     Board board = Board(DEFAULT_MAP,team);
 
     board.print();
-
-    board.doMove(0, 0, 0, 1);
-    std::cout << board.evaluate() << "\n";
-    board.doMove(0, 9, 0, 8);
-    std::cout << board.evaluate() << "\n";
+    std::cout<<board.evaluate()<<std::endl;
+    std::cout<<std::endl;
+//
+//    Piece ep = board.doMove(Move(1,2,1,9));
+//    board.print();
+//    std::cout<<board.evaluate()<<std::endl;
+//    board.undoMove(Move(1,2,1,9),ep);
+//    board.print();
+//    std::cout<<board.evaluate()<<std::endl;
 
     Search ss;
-    Node n = ss.searchMain(board, 1, 1);
+    Node n = ss.searchMain(board, 6, 1);
+    board.doMove(n.move);
+    board.print();
+    std::cout << "score: " << n.score << std::endl;
+    n = ss.searchMain(board, 6, 1);
+    board.doMove(n.move);
+    board.print();
+    std::cout << "score: " << n.score << std::endl;
+    n = ss.searchMain(board, 6, 1);
+    board.doMove(n.move);
+    board.print();
+    std::cout << "score: " << n.score << std::endl;
+    n = ss.searchMain(board, 6, 1);
+    board.doMove(n.move);
+    board.print();
+    std::cout << "score: " << n.score << std::endl;
+    n = ss.searchMain(board, 6, 1);
+    board.doMove(n.move);
+    board.print();
+    std::cout << "score: " << n.score << std::endl;
+    n = ss.searchMain(board, 6, 1);
+    board.doMove(n.move);
+    board.print();
+    std::cout << "score: " << n.score << std::endl;
+    n = ss.searchMain(board, 6, 1);
+    board.doMove(n.move);
+    board.print();
+    std::cout << "score: " << n.score << std::endl;
+    n = ss.searchMain(board, 6, 1);
     board.doMove(n.move);
     board.print();
     std::cout << "score: " << n.score << std::endl;
