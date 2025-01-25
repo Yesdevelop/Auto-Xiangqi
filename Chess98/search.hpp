7a97ba87bbcc84295e54fbfb6e168c228887f716
ForYes <152695110+Yesdevelop@users.noreply.github.com>
2025-01-25 16:49:20 +0800

    修复空着裁剪的bug

--- Chess98/search.hpp
+++ Chess98/search.hpp
@@ -262,15 +262,8 @@ int Search::searchCut(Board &board, int depth, int beta, bool banNullMove)
 
     // 空着裁剪
     std::vector<Piece> pieces = board.getAllLivePieces();
-    int numOfRook = 0;
-    for (const Piece &piece : pieces)
-    {
-        if (piece.pieceid == R_ROOK)
-        {
-            numOfRook++;
-        }
-    }
-    if (!banNullMove && inCheck(board) == false && numOfRook > 0)
+    int currentVl = board.team == RED ? board.vlRed : board.vlBlack;
+    if ((!banNullMove) && inCheck(board) == false && currentVl - 10000 > 600)
     {
         int R = 2;
         board.team = -board.team;
