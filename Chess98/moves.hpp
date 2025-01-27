b911f93be6641b6783d68e0aa9b2f2a837d99cd7
Yesdevelop <yes-noob@outlook.com>
2025-01-27 17:11:38 +0800

    改进着法生成

--- Chess98/moves.hpp
+++ Chess98/moves.hpp
@@ -6,128 +6,106 @@
 class Moves
 {
 public:
-    static MOVES king(TEAM team, Board board, int x, int y);
+    static MOVES king(TEAM team, Board &board, int x, int y);
 
-    static MOVES guard(TEAM team, Board board, int x, int y);
+    static MOVES guard(TEAM team, Board &board, int x, int y);
 
-    static MOVES bishop(TEAM team, Board board, int x, int y);
+    static MOVES bishop(TEAM team, Board &board, int x, int y);
 
-    static MOVES knight(TEAM team, Board board, int x, int y);
+    static MOVES knight(TEAM team, Board &board, int x, int y);
 
-    static MOVES rook(TEAM team, Board board, int x, int y);
+    static MOVES rook(TEAM team, Board &board, int x, int y);
 
-    static MOVES cannon(TEAM team, Board board, int x, int y);
+    static MOVES cannon(TEAM team, Board &board, int x, int y);
 
-    static MOVES pawn(TEAM team, Board board, int x, int y);
+    static MOVES pawn(TEAM team, Board &board, int x, int y);
 
-    static MOVES generateMoves(Board board, int x, int y);
+    static MOVES generateMoves(Board &board, int x, int y);
 
-    static MOVES getMoves(Board board);
+    static MOVES getMoves(Board &board);
 
-    static MOVES getCaptrueMoves(Board board);
+    static MOVES getCaptrueMoves(Board &board);
 
-    static MOVES getGoodCaptures(Board board);
+    static MOVES getGoodCaptures(Board &board);
 };
 
 /// @brief 生成将帅的着法
-MOVES Moves::king(TEAM team, Board board, int x, int y)
+MOVES Moves::king(TEAM team, Board &board, int x, int y)
 {
     MOVES result{};
-    MOVES mayAvailableMoves{
-        Move{x, y, x + 1, y}, Move{x, y, x - 1, y},
-        Move{x, y, x, y + 1}, Move{x, y, x, y - 1}};
+    result.reserve(4);
 
-    for (Move v : mayAvailableMoves)
-    {
-        int x = v.x2;
-        int y = v.y2;
-        if (board.teamOn(x, y) == team)
-        {
-            continue;
-        }
-        if (team == RED)
-        {
-            if (x >= 3 && x <= 5 && y >= 0 && y <= 2)
-            {
-                result.emplace_back(v);
-            }
-        }
-        else
-        {
-            if (x >= 3 && x <= 5 && y >= 7 && y <= 9)
-            {
-                result.emplace_back(v);
-            }
-        }
-    }
-    // 白脸将
+    // 横坐标应当在3, 5之间，纵坐标的话，红方在0, 2之间，黑方在7, 9之间
+    const int left = x - 1;
+    const int right = x + 1;
+    const int up = y + 1;
+    const int down = y - 1;
+
+    if (left >= 3 && board.teamOn(left, y) != team)
+        result.emplace_back(Move{x, y, left, y});
+    if (right <= 5 && board.teamOn(right, y) != team)
+        result.emplace_back(Move{x, y, right, y});
     if (team == RED)
     {
-        for (int _y = y + 1; _y <= 9; _y++)
-        {
-            if (board.teamOn(x, _y) != 0)
-            {
-                if (board.teamOn(x, _y) != team)
-                {
-                    if (board.pieceidOn(x, _y) == R_KING || board.pieceidOn(x, _y) == B_KING)
-                    {
-                        result.emplace_back(Move{x, y, x, _y});
-                    }
-                }
-                break;
-            }
-        }
+        if (up <= 2 && board.teamOn(x, up) != team)
+            result.emplace_back(Move{x, y, x, up});
+        if (down >= 0 && board.teamOn(x, down) != team)
+            result.emplace_back(Move{x, y, x, down});
     }
     else
     {
-        for (int _y = y - 1; _y >= 0; _y--)
-        {
-            if (board.teamOn(x, _y) != 0)
-            {
-                if (board.teamOn(x, _y) != team)
-                {
-                    if (board.pieceidOn(x, _y) == R_KING || board.pieceidOn(x, _y) == B_KING)
-                    {
-                        result.emplace_back(Move{x, y, x, _y});
-                    }
-                }
-                break;
-            }
-        }
+        if (up <= 9 && board.teamOn(x, up) != team)
+            result.emplace_back(Move{x, y, x, up});
+        if (down >= 7 && board.teamOn(x, down) != team)
+            result.emplace_back(Move{x, y, x, down});
     }
 
     return result;
 }
 
 /// @brief 生成士的着法
-MOVES Moves::guard(TEAM team, Board board, int x, int y)
+MOVES Moves::guard(TEAM team, Board &board, int x, int y)
 {
     MOVES result{};
-    MOVES mayAvailableMoves{
-        Move{x, y, x + 1, y + 1}, Move{x, y, x - 1, y - 1},
-        Move{x, y, x - 1, y + 1}, Move{x, y, x + 1, y - 1}};
+    result.reserve(4);
 
-    for (Move v : mayAvailableMoves)
+    // 横坐标也应在3, 5之间，纵坐标的话，红方在0, 2之间，黑方在7, 9之间
+    const int left = x - 1;
+    const int right = x + 1;
+    const int up = y + 1;
+    const int down = y - 1;
+    if (left >= 3)
     {
-        int x = v.x2;
-        int y = v.y2;
-        if (board.teamOn(x, y) == team)
-        {
-            continue;
-        }
         if (team == RED)
         {
-            if (x >= 3 && x <= 5 && y >= 0 && y <= 2)
-            {
-                result.emplace_back(v);
-            }
+            if (board.teamOn(left, up) != team && up <= 2)
+                result.emplace_back(Move{x, y, left, up});
+            if (board.teamOn(left, down) != team && down >= 0)
+                result.emplace_back(Move{x, y, left, down});
         }
         else
         {
-            if (x >= 3 && x <= 5 && y >= 7 && y <= 9)
-            {
-                result.emplace_back(v);
-            }
+            if (board.teamOn(left, up) != team && up <= 9)
+                result.emplace_back(Move{x, y, left, up});
+            if (board.teamOn(left, down) != team && down >= 7)
+                result.emplace_back(Move{x, y, left, down});
+        }
+    }
+    if (right <= 5)
+    {
+        if (team == RED)
+        {
+            if (board.teamOn(right, up) != team && up <= 2)
+                result.emplace_back(Move{x, y, right, up});
+            if (board.teamOn(right, down) != team && down >= 0)
+                result.emplace_back(Move{x, y, right, down});
+        }
+        else
+        {
+            if (board.teamOn(right, up) != team && up <= 9)
+                result.emplace_back(Move{x, y, right, up});
+            if (board.teamOn(right, down) != team && down >= 7)
+                result.emplace_back(Move{x, y, right, down});
         }
     }
 
@@ -135,56 +113,40 @@ MOVES Moves::guard(TEAM team, Board board, int x, int y)
 }
 
 /// @brief 生成象的着法
-MOVES Moves::bishop(TEAM team, Board board, int x, int y)
+MOVES Moves::bishop(TEAM team, Board &board, int x, int y)
 {
     MOVES result{};
-    MOVES mayAvailableMoves{};
-    if (board.pieceidOn(x + 1, y + 1) == EMPTY_PIECEID)
-    {
-        mayAvailableMoves.emplace_back(Move{x, y, x + 2, y + 2});
-    }
-    if (board.pieceidOn(x - 1, y + 1) == EMPTY_PIECEID)
-    {
-        mayAvailableMoves.emplace_back(Move{x, y, x - 2, y + 2});
-    }
-    if (board.pieceidOn(x + 1, y - 1) == EMPTY_PIECEID)
-    {
-        mayAvailableMoves.emplace_back(Move{x, y, x + 2, y - 2});
-    }
-    if (board.pieceidOn(x - 1, y - 1) == EMPTY_PIECEID)
-    {
-        mayAvailableMoves.emplace_back(Move{x, y, x - 2, y - 2});
-    }
+    result.reserve(4);
 
-    for (Move v : mayAvailableMoves)
+    // 横坐标应在0, 9之间，纵坐标的话，红方在0, 4之间，黑方在5, 9之间
+    if (team == RED)
     {
-        int x = v.x2;
-        int y = v.y2;
-        if (board.teamOn(x, y) == team)
-        {
-            continue;
-        }
-        if (team == RED)
-        {
-            if (x >= 0 && x <= 8 && y >= 0 && y <= 4)
-            {
-                result.emplace_back(v);
-            }
-        }
-        else
-        {
-            if (x >= 0 && x <= 8 && y >= 5 && y <= 9)
-            {
-                result.emplace_back(v);
-            }
-        }
+        if (board.teamOn(x - 1, y - 1) == EMPTY_TEAM && board.teamOn(x - 2, y - 2) != team)
+            result.emplace_back(Move{x, y, x - 2, y - 2});
+        if (board.teamOn(x + 1, y - 1) == EMPTY_TEAM && board.teamOn(x + 2, y - 2) != team)
+            result.emplace_back(Move{x, y, x + 2, y - 2});
+        if (board.teamOn(x - 1, y + 1) == EMPTY_TEAM && board.teamOn(x - 2, y + 2) != team && y + 1 <= 4)
+            result.emplace_back(Move{x, y, x - 2, y + 2});
+        if (board.teamOn(x + 1, y + 1) == EMPTY_TEAM && board.teamOn(x + 2, y + 2) != team && y + 1 <= 4)
+            result.emplace_back(Move{x, y, x + 2, y + 2});
+    }
+    else
+    {
+        if (board.teamOn(x - 1, y - 1) == EMPTY_TEAM && board.teamOn(x - 2, y - 2) != team && y - 1 >= 5)
+            result.emplace_back(Move{x, y, x - 2, y - 2});
+        if (board.teamOn(x + 1, y - 1) == EMPTY_TEAM && board.teamOn(x + 2, y - 2) != team && y - 1 >= 5)
+            result.emplace_back(Move{x, y, x + 2, y - 2});
+        if (board.teamOn(x - 1, y + 1) == EMPTY_TEAM && board.teamOn(x - 2, y + 2) != team)
+            result.emplace_back(Move{x, y, x - 2, y + 2});
+        if (board.teamOn(x + 1, y + 1) == EMPTY_TEAM && board.teamOn(x + 2, y + 2) != team)
+            result.emplace_back(Move{x, y, x + 2, y + 2});
     }
 
     return result;
 }
 
 /// @brief 生成马的着法
-MOVES Moves::knight(TEAM team, Board board, int x, int y)
+MOVES Moves::knight(TEAM team, Board &board, int x, int y)
 {
     MOVES result{};
     MOVES mayAvailableMoves{};
@@ -222,7 +184,7 @@ MOVES Moves::knight(TEAM team, Board board, int x, int y)
 }
 
 /// @brief 生成车的着法
-MOVES Moves::rook(TEAM team, Board board, int x, int y)
+MOVES Moves::rook(TEAM team, Board &board, int x, int y)
 {
     MOVES result{};
 
@@ -295,7 +257,7 @@ MOVES Moves::rook(TEAM team, Board board, int x, int y)
 }
 
 /// @brief 生成炮的着法
-MOVES Moves::cannon(TEAM team, Board board, int x, int y)
+MOVES Moves::cannon(TEAM team, Board &board, int x, int y)
 {
     MOVES result{};
 
@@ -412,7 +374,7 @@ MOVES Moves::cannon(TEAM team, Board board, int x, int y)
 }
 
 /// @brief 生成兵的着法
-MOVES Moves::pawn(TEAM team, Board board, int x, int y)
+MOVES Moves::pawn(TEAM team, Board &board, int x, int y)
 {
     if (team == RED)
     {
@@ -492,7 +454,7 @@ MOVES Moves::pawn(TEAM team, Board board, int x, int y)
 }
 
 /// @brief 生成着法
-MOVES Moves::generateMoves(Board board, int x, int y)
+MOVES Moves::generateMoves(Board &board, int x, int y)
 {
     PIECEID chessid = board.pieceidOn(x, y);
     TEAM team = board.teamOn(x, y);
@@ -535,7 +497,7 @@ MOVES Moves::generateMoves(Board board, int x, int y)
 /// @brief 获取当前队伍所有可行着法
 /// @param team
 /// @return
-MOVES Moves::getMoves(Board board)
+MOVES Moves::getMoves(Board &board)
 {
     if (!board.isKingLive(board.team))
     {
@@ -560,7 +522,7 @@ MOVES Moves::getMoves(Board board)
 /// @brief 获取当前队伍所有吃子着法（暂时写成这样，后续优化）
 /// @param board
 /// @return
-MOVES Moves::getCaptrueMoves(Board board)
+MOVES Moves::getCaptrueMoves(Board &board)
 {
     MOVES result{};
     MOVES moves = Moves::getMoves(board);
@@ -577,20 +539,19 @@ MOVES Moves::getCaptrueMoves(Board board)
 /// @brief  获取当前队伍所有好的吃子着法（MVV/LVA）（暂时写成这样，后续优化）
 /// @param board
 /// @return
-MOVES Moves::getGoodCaptures(Board board)
+MOVES Moves::getGoodCaptures(Board &board)
 {
     // SEE
     MOVES result{};
     MOVES moves = Moves::getCaptrueMoves(board);
     const std::map<PIECEID, int> weightPairs{
-            {R_ROOK, 4},
-            {R_CANNON, 3},
-            {R_KNIGHT, 3},
-            {R_BISHOP, 2},
-            {R_GUARD, 2},
-            {R_PAWN, 1},
-            {R_KING, 1}
-    };
+        {R_ROOK, 4},
+        {R_CANNON, 3},
+        {R_KNIGHT, 3},
+        {R_BISHOP, 2},
+        {R_GUARD, 2},
+        {R_PAWN, 1},
+        {R_KING, 1}};
     std::map<int, MOVES> orderMap{};
 
     for (const Move &move : moves)
