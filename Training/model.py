72e92a64f85f1f4b795c9333057f4579af07653b
HeliumAreFlying <1436980420@qq.com>
2025-01-31 15:01:22 +0800

    perfect the model.py

--- Training/model.py
+++ Training/model.py
@@ -31,23 +31,38 @@ class nnue(nn.Module):
         return y1,y2
 
     def convert_board_to_x(self,board : np.ndarray,side):
-        inputs = np.zeros(shape=7 * 90 + 1,dtype=np.float32)
+        input = np.zeros(shape=7 * 90 + 1,dtype=np.float32)
         if side == Black:
-            inputs[-1] = 1
+            input[-1] = 1
         for x in range(9):
             for y in range(10):
                 p = board[x][y]
                 if p < 0:
-                    inputs[(abs(p) - 1) * 90 + x * 10 + y] = -1
+                    input[(abs(p) - 1) * 90 + x * 10 + y] = -1
                 elif p > 0:
-                    inputs[(p - 1) * 90 + x * 10 + y] = 1
-        inputs = torch.from_numpy(inputs)
+                    input[(p - 1) * 90 + x * 10 + y] = 1
+        input = torch.from_numpy(input)
+        return input.to(public_device)
+
+    def convert_boards_to_xs(self,boards,sides):
+        inputs = []
+        for i in range(len(boards)):
+            inputs.append(self.convert_board_to_x(boards[i],sides[i]))
+        inputs = torch.stack(inputs,dim=0)
+        print(inputs.shape)
         return inputs.to(public_device)
 
 if __name__ == "__main__":
     board = copy.deepcopy(init_board)
     model = nnue().to(public_device)
-    inputs = model.convert_board_to_x(board,Red)
-    vl,policy = model(inputs)
+    input = model.convert_board_to_x(board,Red)
+    vl,policy = model(input)
     print(vl)
     print(policy)
+
+    inputs,input_sides,vls,move_ids = get_data(r"D:\dump_3\split_1\0.txt")
+    inputs = model.convert_boards_to_xs(inputs,input_sides)
+    vls,policies = model(inputs)
+    print(vls)
+    print(policies)
+    print(policies.shape)
