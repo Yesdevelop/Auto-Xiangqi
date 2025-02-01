438e497da7e867de5f4b6445e94aad47be78e9c0
HeliumAreFlying <1436980420@qq.com>
2025-02-01 08:55:58 +0800

    fix bug of repeated file name

--- Chess98/traverse.cpp
+++ Chess98/traverse.cpp
@@ -1,10 +1,13 @@
-#include "test.hpp"
 #include <iostream>
 #include <random>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
 #include <io.h>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include "test.hpp"
 
 /* ***** 跑谱器 ***** */
 
@@ -33,18 +36,50 @@ void getFiles(std::string path, std::vector<std::string>& files)
     }
 }
 
+// 检查文件是否存在的函数
+bool fileExists(const std::string& filename) {
+    std::ifstream file(filename);
+    return file.good();
+}
+
+// 生成基于当前日期和时间以及随机数的唯一文件名的函数
+std::string generateUniqueFilename(const std::string& outputPath) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 9999);
+
+    std::string filename;
+    do {
+        // 获取当前时间
+        auto now = std::chrono::system_clock::now();
+        auto in_time_t = std::chrono::system_clock::to_time_t(now);
+        auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() % 1000;
+
+        // 生成随机数
+        int random_number = dis(gen);
+
+        // 组合时间戳和随机数生成文件名
+        std::stringstream ss;
+        ss << outputPath << std::put_time(std::localtime(&in_time_t), "%Y%m%d%H%M%S") << millis << "_" << random_number << ".txt";
+        filename = ss.str();
+
+    } while (fileExists(filename)); // 如果文件已经存在，重新生成
+
+    return filename;
+}
+
 void traverse() {
     std::string rootPath = "../need_to_estimate";
     std::vector<std::string> filePaths;
     getFiles(rootPath, filePaths);
     std::cout << filePaths.size() << std::endl;
-    for (int cnt = 0;cnt < filePaths.size();cnt++) {
+    for (int cnt = 0; cnt < filePaths.size(); cnt++) {
         std::string path = filePaths[cnt];
         // init status
         Board board = Board(DEFAULT_MAP, RED);
         Search s;
-        s.searchInit(board,16);
-        // prepare to read recoards
+        s.searchInit(board, 16);
+        // prepare to read records
         std::ifstream in(path);
         std::string moveStr;
         std::vector<std::string> dumps;
@@ -54,7 +89,7 @@ void traverse() {
             std::string mv = std::to_string(combinatePos);
             // estimate the game status
             Node BestNode = s.searchMain(board, 6, 1);
-            // recoard the move with assessment result
+            // record the move with assessment result
             std::string output = mv + " " + std::to_string(BestNode.score);
             dumps.push_back(output);
             // next step
@@ -67,8 +102,7 @@ void traverse() {
             Move tMove = Move(xSrc, ySrc, xDst, yDst);
             board.doMove(tMove);
         }
-        std::string filename = std::to_string(cnt) + ".txt";
-        std::string filepath = "../assessment_result/" + filename;
+        std::string filepath = generateUniqueFilename("../assessment_result/");
         std::ofstream outfile;
         outfile.open(filepath);
         for (std::string& output : dumps) {
@@ -80,19 +114,19 @@ void traverse() {
 
 void randomTraverse() {
     std::string outputPath = "../dump/";
-    for (int i = 0;i < 1000000;i++) {
+    for (int i = 0; i < 1000000; i++) {
         Board board = Board(DEFAULT_MAP, RED);
         Search s;
         s.searchInit(board, 20);
         std::vector<std::string> dumps;
         std::srand(time(0));
-        for (int a = 0;a < 200;a++) {
+        for (int a = 0; a < 200; a++) {
             int vlRandom = rand() % 100;
             std::string mv;
             std::string vl;
             std::string flag;
             Move tMove;
-            if (vlRandom <= 10) {
+            if (vlRandom <= 20) {
                 MOVES mvs = Moves::getMoves(board);
                 MOVES valid_mvs;
                 for (const Move& move : mvs) {
@@ -129,13 +163,13 @@ void randomTraverse() {
             std::string output = mv + " " + vl + " " + flag;
             dumps.emplace_back(output);
             board.doMove(tMove);
-            // check end game
+            // 检查是否结束游戏
             if (board.isKingLive(RED) == false || board.isKingLive(BLACK) == false) {
                 break;
             }
         }
         std::ofstream outfile;
-        std::string filepath = outputPath + std::to_string(rand() * rand() * rand()) + ".txt";
+        std::string filepath = generateUniqueFilename(outputPath);
         outfile.open(filepath);
         for (std::string& output : dumps) {
             outfile << output << std::endl;
@@ -143,8 +177,6 @@ void randomTraverse() {
         outfile.close();
     }
 }
-
-
 
 int main()
 {
