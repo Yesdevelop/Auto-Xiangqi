025e58507d8df93aea1571bf1e770072e5187194
HeliumAreFlying <1436980420@qq.com>
2025-06-02 19:03:14 +0800

    history table

--- Chess98/search.hpp
+++ Chess98/search.hpp
@@ -280,6 +280,7 @@ Result Search::searchRoot(Board &board, int depth)
     else
     {
         this->pHistoryCache->add(bestMove, depth);
+        this->pHashTable->add(board, bestMove);
     }
     Result result{bestMove.id == -1 ? Move{} : bestMove, vlBest};
 
