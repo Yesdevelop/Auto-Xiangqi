b6b11a8c2cd2e0aba6e987fd83e33421f5f73f5b
ForYes <yes-noob@outlook.com>
2025-06-26 20:35:43 +0800

    hidden window style

--- Chess98/ui.hpp
+++ Chess98/ui.hpp
@@ -121,7 +121,7 @@ void ui(std::string serverDir, TEAM team, int maxDepth, int maxTime, std::string
     board.print();
 
     // 界面
-    std::string cmd = "powershell.exe -command \"& {Start-Process -WindowStyle min node " + serverDir + "}\"";
+    std::string cmd = "powershell.exe -command \"& {Start-Process -WindowStyle hidden node " + serverDir + "}\"";
     system(cmd.c_str());
     setBoardCode(board);
     std::string moveFileContent = "____";
