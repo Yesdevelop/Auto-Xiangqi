99d27e1818ca36dd3bb2a88b8450092c8affab44
ForYes <yes-noob@outlook.com>
2025-07-25 17:00:25 +0800

    aiFirst

--- Chess98/test.hpp
+++ Chess98/test.hpp
@@ -5,7 +5,7 @@ void testByUI()
 {
     std::string serverDir = "../Tools/UI/server.js";
     TEAM team = RED;
-    bool aiFirst = false;
+    bool aiFirst = true;
     int maxDepth = 20;
     int maxTime = 3;
     std::string fenCode = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1";
