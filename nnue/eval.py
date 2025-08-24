ad9abeae69931271c47deb9e7cb23e19bdef260f
ForYes <yes-noob@outlook.com>
2025-08-24 22:47:26 +0800

    eval cases add

--- nnue/eval.py
+++ nnue/eval.py
@@ -32,10 +32,30 @@ def evaluate(fen):
 # 示例
 if __name__ == "__main__":
     os.system("cls")
-    fen = "1Cbakr3/4a4/2R1b4/p1p1c1p1p/9/2P1P4/P7P/9/4A4/2BK1Ar2 w - - 0 1"
-    fen = "1Cbak4/4a4/4R4/p1p1c1p1p/9/2P1P4/P4r2P/9/4A4/2BK1Ar2 w - - 0 1"
-    fen = "1Cbak4/9/3a5/p1p1c1p1p/9/2P1P4/P4r2P/9/4A4/2BK1Ar2 w - - 0 1"
-    # fen = "2bak4/9/3a5/p1p1c1p1p/9/2P1P4/P4r2P/9/4A4/2BK1Ar2 w - - 0 1"
-    # fen = "2bak4/1C7/3a5/p1p1c1p1p/9/2P1P4/P4r2P/9/4A4/2BK1Ar2 w - - 0 1"
-    score = -evaluate(fen) # 我的队伍还是搞反了，需要反过来获取分数
-    print(f"局面评估值: {score:.2f} (红方视角)")
+    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/1NBAKABNR w - - 0 1" # 让单车
+    score1 = -evaluate(fen) # 我的队伍还是搞反了，需要反过来获取分数
+    print(f"让单车，评估分数: {score1:.2f}")
+    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/7C1/9/RNBAKABNR w - - 0 1" # 让单炮
+    score2 = -evaluate(fen)
+    print(f"让单炮，评估分数: {score2:.2f}")
+    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/R1BAKABNR w - - 0 1" # 让单马
+    score3 = -evaluate(fen)
+    print(f"让单马，评估分数: {score3:.2f}")
+    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1" # 不让
+    score4 = -evaluate(fen)
+    print(f"不让，评估分数: {score4:.2f}")
+    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/9/1C5C1/9/RNBAKABNR w - - 0 1" # 让全兵
+    score5 = -evaluate(fen)
+    print(f"让全兵，评估分数: {score5:.2f}")
+    fen = "rnbakabnr/9/9/p1p1p1p1p/9/9/P1P1P1P1P/9/9/RN2K2NR w - - 0 1" # 让士相
+    score6 = -evaluate(fen)
+    print(f"让士相，评估分数: {score6:.2f}")
+    fen = "rnbakabnr/9/9/p1p1p1p1p/9/9/9/9/9/RN5NR w - - 0 1" # 缺帅
+    score7 = -evaluate(fen)
+    print(f"缺帅，评估分数: {score7:.2f}")
+    fen = "9/9/9/9/9/9/9/9/9/9 w - - 0 1" # 空盘
+    score8 = -evaluate(fen)
+    print(f"空盘，评估分数: {score8:.2f}")
+    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR b - - 0 1" # 红让双车，黑让单炮
+    score9 = evaluate(fen) # 黑方视角
+    print(f"红让双车，黑让单炮，评估分数: {score9:.2f}")
