f5b69fb20e9bb4583fd0b8818436fddf3717e06c
HeliumAreFlying <1436980420@qq.com>
2025-08-24 23:14:48 +0800

    fens

--- nnue/eval.py
+++ nnue/eval.py
@@ -17,40 +17,22 @@ model.to(device)
 model.eval()
 
 def evaluate(fen):
-    """
-    输入FEN字符串，返回模型评估值（红方视角，单位与训练一致）
-    """
     sit = Situation(fen)
     input_tensor = torch.tensor(sit.matrix.copy(), dtype=torch.float32).view(1, -1).to(device)
     with torch.no_grad():
         output = model(input_tensor)  # [1, 2]
     flag = Red if 'w' in fen else Black
-    score = output[0, flag].item() * 1000
+    score = output[0, 1 - flag].item() * 1000
     return score
 
 # 示例
 if __name__ == "__main__":
-    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/1NBAKABNR w - - 0 1" # 让单车
-    score1 = -evaluate(fen) # 我的队伍还是搞反了，需要反过来获取分数
-    print(f"让单车，评估分数: {score1:.2f}")
-    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/7C1/9/RNBAKABNR w - - 0 1" # 让单炮
-    score2 = -evaluate(fen)
-    print(f"让单炮，评估分数: {score2:.2f}")
-    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/R1BAKABNR w - - 0 1" # 让单马
-    score3 = -evaluate(fen)
-    print(f"让单马，评估分数: {score3:.2f}")
-    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1" # 不让
-    score4 = -evaluate(fen)
-    print(f"不让，评估分数: {score4:.2f}")
-    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/9/1C5C1/9/RNBAKABNR w - - 0 1" # 让全兵
-    score5 = -evaluate(fen)
-    print(f"让全兵，评估分数: {score5:.2f}")
-    fen = "rnbakabnr/9/9/p1p1p1p1p/9/9/P1P1P1P1P/9/9/RN2K2NR w - - 0 1" # 让士相
-    score6 = -evaluate(fen)
-    print(f"让士相，评估分数: {score6:.2f}")
-    fen = "rnbakabnr/9/9/p1p1p1p1p/9/9/9/9/9/RN5NR w - - 0 1" # 缺帅
-    score7 = -evaluate(fen)
-    print(f"缺帅，评估分数: {score7:.2f}")
-    fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR b - - 0 1" # 红让双车，黑让单炮
-    score8 = -evaluate(fen)
-    print(f"红让双车，黑让单炮，评估分数: {score8:.2f}")
+    fens = [
+        ["rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C2C4/9/RNBAKABNR b - - 0 1","炮二平五"],
+        ["rnbakab1r/9/1c5cn/p1p1p1p1p/9/9/P1P1P1P1P/1C2C4/9/RNBAKABNR w - - 0 1","炮二平五->马8进9"],
+        ["rnbakab1r/9/1c4nc1/p1p1p1p1p/9/9/P1P1P1P1P/1C2C4/9/RNBAKABNR w - - 0 1","炮二平五->马8进7"],
+        ["rnbakab1r/9/1c5cn/p1p1C1p1p/9/9/P1P1P1P1P/1C7/9/RNBAKABNR b - - 0 1","炮二平五->马8进9->炮五进四"],
+    ]
+    for fen,info in fens:
+        score = evaluate(fen)
+        print(f'{info} => {score:.2f}')
